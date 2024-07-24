1134e28023856087c6b189ffe81e1b6e2ce8449f
Matt Young <matt.young.1@outlook.com>
2024-07-24 19:11:49 +1000

    Change formatting, add finalise pass

---
+++ src/tamara_finalise_pass.cpp
@@ -0,0 +1,36 @@
+#include "kernel/log.h"
+#include "kernel/register.h"
+#include "kernel/yosys_common.h"
+
+USING_YOSYS_NAMESPACE
+
+PRIVATE_NAMESPACE_BEGIN
+
+/**
+ * The tmr_finalise command, used to "unblackbox" the modules that were blackboxed in the tmr command.
+ */
+struct TamaraFinalisePass : public Pass {
+
+    TamaraFinalisePass() : Pass("tmr_finalise", "Finalises the TaMaRa TMR pipeline") {
+    }
+
+    void help() override {
+        //   |---v---|---v---|---v---|---v---|---v---|---v---|---v---|---v---|---v---|---v---|
+        log("\n");
+        log("    tmr_finalise\n");
+        log("\n");
+
+        log("Finalises the TaMaRa automated TMR process by unpacking blackboxes.\n");
+        log("This command should be run after 'tmr'\n\n");
+    }
+
+    void execute(std::vector<std::string> args, RTLIL::Design *design) override {
+        log_header(design, "Finalising TaMaRa automated triple modular redundancy\n\n");
+        log_push();
+
+        log_pop();
+    }
+
+} const TamaraFinalisePass;
+
+PRIVATE_NAMESPACE_END
--- src/tamara_tmr_pass.cpp
+++ src/tamara_tmr_pass.cpp
@@ -7,47 +7,33 @@ USING_YOSYS_NAMESPACE
 PRIVATE_NAMESPACE_BEGIN
 
 /**
- * This is the main TaMaRa TMR command, and so is the main entry point to the TMR process.
+ * This is the main TaMaRa TMR command, which starts the TMR process.
  */
 struct TamaraTmrPass : public Pass {
 
-    TamaraTmrPass()
-        : Pass("tmr", "Starts TaMaRa automated TMR pipeline") {
+    TamaraTmrPass() : Pass("tmr", "Starts TaMaRa automated TMR pipeline") {
     }
 
     void help() override {
         //   |---v---|---v---|---v---|---v---|---v---|---v---|---v---|---v---|---v---|---v---|
         log("\n");
-        log("    tmr [modules...]\n");
+        log("    tmr\n");
         log("\n");
 
-        log("TaMaRa is an automated Triple Modular Redundancy flow for Yosys.\n\n");
-
-        log("If [modules...] is empty, TaMaRa will apply TMR to all selected modules.\n");
-        log("Otherwise, TMR will be applied only to the modules specified.\n\n");
+        log("TaMaRa is an automated triple modular redundancy flow for Yosys.\n");
+        log("The 'tmr' command will process all the selected modules in the design,\n");
+        log("and apply TMR to each one.\n\n");
 
         log("The 'tmr' command should be run after synthesis but before techmapping.\n");
-        log("Later, you need to run 'tmr_finalise' to complete the TMR process.\n\n");
+        log("Later, you must run 'tmr_finalise' to complete the TMR process.\n\n");
     }
 
     void execute(std::vector<std::string> args, RTLIL::Design *design) override {
-        log_header(design, "Starting TaMaRa automated Triple Modular Redundancy\n\n");
+        log_header(design, "Starting TaMaRa automated triple modular redundancy\n\n");
         log_push();
 
-        std::vector<const RTLIL::Module*> modules;
-
-        if (args.size() <= 1) {
-            // use design selected modules
-            for (auto *const module : design->selected_modules()) {
-                modules.push_back(module);
-            }
-        } else {
-            // lookup modules by args
-            // TODO note starting at idx 1
-            log_error("Selecting module name by args is not yet supported!\n");
-        }
-
-        for (const auto &module : modules) {
+        // process each selected module from the design
+        for (auto *const module : design->selected_modules()) {
             log_push();
 
             log_header(design, "Applying TMR to module: %s\n", log_id(module->name));
