fccbc4a8a6727cc96b35822bbef01b6dcf68d71c
Matt Young <matt.young.1@outlook.com>
2024-09-04 02:48:14 +1000

    Add ecp5_shotgun idea before I forget

---
+++ tools/ecp5_shotgun.cpp
@@ -0,0 +1,8 @@
+// TaMaRa: An automated triple modular redundancy EDA flow for Yosys.
+// Copyright (c) 2024 Matt Young.
+
+// ecp5_shotgun: A fault injection tool for ECP5 bitstreams.
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
