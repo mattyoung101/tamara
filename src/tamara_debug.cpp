62c02fc839a3924581fd2bcd2f982982f4abc8b5
Matt Young <matt.young.1@outlook.com>
2024-09-04 20:34:29 +1000

    Start on tamara::VoterBuilder

---
+++ src/tamara_debug.cpp
@@ -0,0 +1,59 @@
+// TaMaRa: An automated triple modular redundancy EDA flow for Yosys.
+//
+// Copyright (c) 2024 Matt Young.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#include "kernel/log.h"
+#include "kernel/register.h"
+#include "kernel/rtlil.h"
+#include "kernel/yosys_common.h"
+#include "tamara/voter_builder.hpp"
+#include <vector>
+
+USING_YOSYS_NAMESPACE
+
+PRIVATE_NAMESPACE_BEGIN
+
+//! The tamara_debug command is used for debugging various TaMaRa features.
+struct TamaraDebug : public Pass {
+
+    TamaraDebug() : Pass("tamara_debug", "Used to debug various TaMaRa features") {
+    }
+
+    void help() override {
+        //   |---v---|---v---|---v---|---v---|---v---|---v---|---v---|---v---|---v---|---v---|
+        log("\n");
+        log("    tamara_debug [task]\n");
+        log("\n");
+
+        log("Executes the debug task specified.\n");
+    }
+
+    void execute(std::vector<std::string> args, RTLIL::Design *design) override {
+        log_header(design, "Running TaMaRa debug task\n\n");
+        log_push();
+
+        if (args.size() <= 1) {
+            log_error("Must specify debug task.\n");
+        }
+
+        const auto task = args[1];
+
+        // sadly we can't switch on strings
+        if (task == "forcePropagateDone") {
+            log("Forcing propagate to be marked as done\n");
+            design->scratchpad_set_bool("tamara_propagate.didRun", true);
+        } else if (task == "mkvote") {
+            log("Generating one voter\n");
+            tamara::VoterBuilder::build(design);
+            // TODO
+        } else {
+            log_error("Unhandled debug task: '%s'\n", task.c_str());
+        }
+
+        log_pop();
+    }
+} const TamaraDebug;
+
+PRIVATE_NAMESPACE_END
--- src/voter_builder.cpp
+++ src/voter_builder.cpp
@@ -4,19 +4,14 @@
 //
 // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL
 // was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
-#pragma once
 #include "kernel/rtlil.h"
 #include "kernel/yosys_common.h"
+#include "tamara/voter_builder.hpp"
 
 USING_YOSYS_NAMESPACE
 
-namespace tamara {
-
-//! Used to build and insert voters into a Yosys RTLIL design.
-class VoterBuilder {
-public:
-    //! Insert one voter into the design.
-    static void build(RTLIL::Design *design);
-};
-
-}; // namespace tamara
+void tamara::VoterBuilder::build(RTLIL::Design *design) {
+    // generate voter module with a unique name
+    // TODO we may not want to generate a module?
+    auto *module = design->addModule(NEW_ID_SUFFIX("tamara_voter"));
+}
